7b143987db2d318db69b080e536b1f2fecaaf39f
inayat100 <68042481+inayat100@users.noreply.github.com>
2022-03-14 01:54:35 +0530

    Update views.py
    ok

--- app/views.py
+++ app/views.py
@@ -3,6 +3,7 @@ from .forms import detail_form,user_form,singin_form
 from .models import user_details
 from django.contrib.auth.models import User
 from  django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 # Create your views here.
 def singup(request):
     if request.method == 'POST':
@@ -15,6 +16,9 @@ def singup(request):
         user_type = request.POST['User_Type']
         image = request.FILES['image']
         address = request.POST['address']
+        if password1 != password2:
+            messages.error(request,"both password is not same..")
+            return redirect('/')
         us1 = User.objects.create_user(user, email, password1)
         us1.first_name=fname
         us1.last_name=lname
@@ -22,6 +26,7 @@ def singup(request):
         user = User.objects.get(username=user)
         us2 = user_details(user=user,User_Type=user_type, image=image, address=address)
         us2.save()
+        messages.success(request,"singup successfully")
         return redirect('singin')
     return render(request,'singup.html',{'user_form':user_form,'detail_form':detail_form})
 
@@ -29,15 +34,15 @@ def singin(request):
     if request.method == "POST":
         username = request.POST['User_name']
         password = request.POST['password']
-        print(username,password)
         user = authenticate(username=username, password=password)
-        print(user)
         if user:
             login(request, user)
             us = User.objects.get(username = request.user)
             us1 = user_details.objects.get(user= request.user)
+            messages.success(request, "login successfully")
             return render(request,'dash.html',{'user':us,'detail':us1})
         else:
+            messages.error(request, "something is wrong input")
             HttpResponseRedirect('/')
     else:
         fm = singin_form
